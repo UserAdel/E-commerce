d1fb21c4c2ee9af3c2ba658a0981fd59e9f61003
adel hany <adel58420@gmail.com>
2025-03-25 02:33:00 +0200

    Login and Register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Userlayout from "./components/Layout/Userlayout";
 import Home from "../src/components/Pages/Home";
 import Login from "./components/Pages/Login";
+import Register from "./components/Pages/Register"; 
 import { Toaster } from 'sonner';
 
 
@@ -13,6 +14,7 @@ function App() {
       <Route path="/" element={<Userlayout />}>
         <Route index element={<Home />} />
         <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
 
       </Route>
     </Routes>
--- client/src/components/Pages/Login.jsx
+++ client/src/components/Pages/Login.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from "react";
+import login from "../../assets/login.webp";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>
-      LoginPage
-    </div>
-  )
-}
 
-export default Login
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const submitHanler = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+    console.log(email, password)
+  };
+  return (
+    <div className="flex">
+      <div className="md:w-1/2 w-full flex justify-center items-center ">
+        <form
+          onSubmit={submitHanler}
+          className="flex flex-col border  shadow-md p-10 w-full max-w-md rounded-md"
+        >
+          <h2 className="text-xl text-center mb-4">Rabbit</h2>
+          <h1 className="text-3xl font-bold mb-4 text-center"> Hey there! </h1>
+          <h1 className="text-lg text-gray-800 mb-4 font-semibold text-center">Enter your email and password to login</h1>
+          <label htmlFor="email" className="mb-4"> Email</label>
+          <input type="email" placeholder="enter your email"
+          className="border p-2 mb-4"
+          onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="password" className="mb-4"> Password</label>
+          <input type="password" placeholder="enter your password " onChange={(e)=>setPassword(e.target.value)}  className="border p-2 mb-4" />
+          <button type="submit" className="bg-black mt-4 text-white p-2 rounded">
+            Login
+          </button>
+
+          <div className="flex justify-center items-center  mt-4">
+            <h1 className=" text-gray-600 text-center text-lg"> don't have an account? </h1>
+            <Link to="/register" className="text-blue-500 hover:underline ml-1" > Register</Link>
+          </div>
+        </form>
+      </div>
+
+      {/* image section */}
+      <div className="w-1/2 hidden md:flex">
+        <img src={login} alt="" className="h-[750px] w-full object-cover" />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
