0128c3b8103ce75b1d06111828d10ad4b3fa43ec
adel hany <adel58420@gmail.com>
2025-03-27 17:08:15 +0200

    SortOptions

--- client/src/components/Products/FilterSidebar.jsx
+++ client/src/components/Products/FilterSidebar.jsx
@@ -65,37 +65,41 @@ const FilterSidebar = () => {
     setPriceRange([prams.minPrice || 0, prams.maxPrice || 100]);
   }, [searchParams]);
 
-  
-const updateSearchParams = (newfilters) => {
-  const params = new URLSearchParams();
-  Object.keys(newfilters).forEach((key) => {
-    if (Array.isArray(newfilters[key]) && newfilters[key].length) {
-      params.append(key, newfilters[key].join(","));
-    }
-    else {
-      params.append(key, newfilters[key]);
-    }
-
-  });
-  setsearchParams(params);
-  navigate(`?${params.toString()}`);
+  const updateSearchParams = (newfilters) => {
+    const params = new URLSearchParams();
+    Object.keys(newfilters).forEach((key) => {
+      if (Array.isArray(newfilters[key]) && newfilters[key].length) {
+        params.append(key, newfilters[key].join(","));
+      } else {
+        params.append(key, newfilters[key]);
+      }
+    });
+    setsearchParams(params);
+    navigate(`?${params.toString()}`);
+  };
+const handlePriceRangeChange = (e) => {
+  const newPrice = e.target.value;
+  const newfilter = { ...filter, minPrice: 0, maxPrice: newPrice }; 
+  setFilter(newfilter);
+  updateSearchParams(newfilter);
 };
 
-
-
   const handleFilterChange = (e) => {
-    const { name, value, checked } = e.target;
+    const { name, value, checked, type } = e.target;
     let newfilter = { ...filter };
-    if (checked) {
+    if (type === "checkbox") {
+      if (checked) {
         newfilter[name] = [...newfilter[name], value];
-
-    } else{newfilter[name] =newfilter[name].filter((item)=>item!==value)
-
+      } else {
+        newfilter[name] = newfilter[name].filter((item) => item !== value);
+      }
+    } else {
+      newfilter[name] = value;
     }
-    console.log(newfilter)
+
     setFilter(newfilter);
     updateSearchParams(newfilter);
-  }
+  };
 
   return (
     <div className="p-4">
@@ -110,10 +114,9 @@ const updateSearchParams = (newfilters) => {
                 type="radio"
                 name="category"
                 value={category}
+                checked={filter.category === category}
                 className="size-4 mt-2"
                 onChange={handleFilterChange}
-                
-                
               />
               <label className="ml-2 text-lg">{category}</label>
             </div>
@@ -131,6 +134,7 @@ const updateSearchParams = (newfilters) => {
                 name="gender"
                 value={genders}
                 className="size-4 mt-2"
+                checked={filter.gender === genders}
                 onChange={handleFilterChange}
               />
               <label className="ml-2 text-lg">{genders}</label>
@@ -143,8 +147,16 @@ const updateSearchParams = (newfilters) => {
         <label className=" text-xl">colors </label>
         <div className="flex flex-wrap ">
           {colors.map((color) => (
-            <button key={color}  name="color" onChange={handleFilterChange}  style={{ backgroundColor: color.toLowerCase() }} className=" mr-5 mt-2 w-8 h-8 rounded-full border-2 flex items-center justify-center">
-            </button>
+            <button
+              key={color}
+              name="color"
+              value={color}
+              onClick={handleFilterChange}
+              style={{ backgroundColor: color.toLowerCase() }}
+              className={`mr-5 mt-2 w-8 h-8 hover:scale-105 cursor-pointer rounded-full border-2 flex items-center justify-center ${
+                filter.color === color ? "ring-2 ring-blue-500" : ""
+              }`}
+            />
           ))}
         </div>
       </div>
@@ -158,6 +170,7 @@ const updateSearchParams = (newfilters) => {
                 type="checkbox"
                 name="size"
                 value={sizes}
+                checked={filter.size.includes(sizes)}
                 className="size-4 focus:ring-blue-400 border-gray-300"
                 onChange={handleFilterChange}
               />
@@ -167,50 +180,47 @@ const updateSearchParams = (newfilters) => {
         </div>
       </div>
 
-
-
       <div className="mb-4 ">
-        <label className=" text-xl ">brands </label>
+        <label className=" text-xl ">materials </label>
         <div className="flex flex-col mt-2">
-          {brands.map((brand) => (
-            <div key={brand} className="flex items-center ">
+          {materials.map((material) => (
+            <div key={material} className="flex items-center ">
               <input
                 type="checkbox"
-                name="brand"
-                value={brand}
+                name="material"
+                value={material}
                 className="size-4 "
+                checked={filter.material.includes(material)}
                 onChange={handleFilterChange}
               />
-              <label className="ml-2 mt-1 text-lg focus:ring-blue-400 border-gray-300">{brand}</label>
+              <label className="ml-2 mt-1 text-lg focus:ring-blue-400 border-gray-300">
+                {material}
+              </label>
             </div>
           ))}
         </div>
       </div>
 
-<div className="mb-4">
-<div className="block text-gray-600 font-medium mb-2">
-<label>
-  <input
-    type="range"
-    name="priceRange"
-    min={0}
-    max={100}
-    className="w-full h-2 bg-gray-300 rounded-lg appearance-none cursor-pointer"
-  />
-</label>
+      <div className="mb-4">
+        <div className="block text-gray-600 font-medium mb-2">
+          <label>
+            <input
+              type="range"
+              name="priceRange"
+              min={0}
+              value={priceRange[1]}
+              onChange={handlePriceRangeChange}
+              max={100}
+              className="w-full h-2 bg-gray-300 rounded-lg appearance-none cursor-pointer"
+            />
+          </label>
 
-<div className="flex justify-between text-gray-600 mt-2">
-  <span>$0</span>
-  <span>${priceRange[1]}</span>
-</div>
-
-</div>
-</div>
-
-
-
-
-
+          <div className="flex justify-between text-gray-600 mt-2">
+            <span>$0</span>
+            <span>${priceRange[1]}</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/components/Products/SortOptions.jsx
+++ client/src/components/Products/SortOptions.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 const SortOptions = () => {
+  const [searchParams, setSearchParams] = useSearchParams()
+const handelSortChange = (e) => {
+  const sortBy=e.target.value;
+  searchParams.set("SortBy",sortBy);
+  setSearchParams(searchParams);
+}
   return (
-    <div>
-      Sort options
-    </div>
+    <div className='mb-4 flex items-center justify-end'> 
+    <select  id="Sort" onChange={handelSortChange} value={searchParams.get("SortBy") || ""} className='border p-2 roundede-md focus:outline-none'>
+      <option value="">Default </option>
+      <option value="priceAsc">Price Low to High </option>
+      <option value="priceDesc"> Price High to Low</option>
+      <option value="popularity">Popularity </option>      
+       </select>
+
+     </div>
   )
 }
 
